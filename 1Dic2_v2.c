feb74ca430527f459477e9749237326a5297cf71
David Rodríguez <71563428+Darodbas@users.noreply.github.com>
2025-12-01 23:17:49 +0100

    Add files via upload

---
+++ 1Dic2_v2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main() {
+
+int n = 50;
+int cont = 0;
+char line[256];
+char direction = 'M';
+int num = 0;
+int n0 = 50;
+int i = 0;
+
+  FILE *file = fopen("1Dic.txt", "r");
+  
+  // Check if file opened successfully
+  if (file == NULL) {
+    printf("Error opening file\n");
+    return 1;
+  }
+
+  // Read each line from the file
+  while (fgets(line, sizeof(line), file) != NULL) {
+    // Remove newline character if present
+    line[strcspn(line, "\n")] = 0;
+    
+    // Extract first character and the following number
+    char direction = line[0];
+    int num = atoi(&line[1]);
+
+    cont += (int) floor((double) num/100);
+    num = num % 100;
+
+
+    if(direction == 'L'){
+      n = n - num;
+        if(n<0){
+          n = 100 + n;
+          if(n0 > 0){
+            cont++;
+          }
+        }
+    }
+
+    else{
+      n = n + num;
+        if(n>99){
+          n = n % 100;
+          if (n>0){
+          cont++;
+          }
+        }
+    }
+
+    if(n==0){
+      cont++;
+    }
+
+    n0 = n;
+
+    printf("Line: %s \t| Direction: %c  \t| N: %d  \t| Cont: %d\n", line, direction, n, cont);
+
+  }
+  
+  fclose(file);
+  printf("\n\nTotal: %d\n", cont);
+  return 0;
+}
